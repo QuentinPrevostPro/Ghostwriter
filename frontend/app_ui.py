89313e3df2d3640c8382290512c9b1308aaec583
Quentin Prevost <quentinprevost@MacBook-Pro-de-Quentin.local>
2025-12-10 12:07:12 +0100

    first UI with streamlit

---
+++ frontend/app_ui.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import requests
+from app.db.vector_store import VectorStore    
+
+
+st.title("Ghostwriter")
+
+#Parameters
+top_k = 5
+table_name = "content"
+
+# Input fields
+query = st.text_area("Enter your text or idea")
+store = VectorStore("./app/db/ghostwriter_db")
+authors = store.list_authors("content")
+author = st.selectbox("Select author", authors)
+
+# Generate button
+if st.button(f"Write like {author}"):
+    if not query or not author:
+        st.error("Please enter both query and author")
+    else:
+        # Call the FastAPI endpoint
+        try:
+            response = requests.post(
+                "http://127.0.0.1:8000/generate",
+                json={
+                    "query": query,
+                    "author": author,
+                    "top_k": top_k,
+                    "table_name": table_name
+                }
+            )
+            response.raise_for_status()
+            data = response.json()
+            st.text_area("Generated Response", value=data["response"], height=300)
+        except Exception as e:
+            st.error(f"Error calling API: {e}")
